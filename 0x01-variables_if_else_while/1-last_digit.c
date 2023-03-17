333f995a4df68b2249fa54861fb4073290e64173
Akinbami Taofeek Olamide <tfkakinbami80@yahoo.com>
2023-03-17 15:00:58 -0700

    A code that pprint the last digit of a given random number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - A code that prints the last digit of a randon number.
+ * Author - Akinbami Taofeek Olamide
+ * Return: Return zero if successfully executed
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n % 10 > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else if ((n % 10 < 6) && (n % 10 != 0))
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	return (0);
+}
+
