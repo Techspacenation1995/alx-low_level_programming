ba2cba6650d5389a1015b05712b2733933b27806
Akinbami Taofeek Olamide <tfkakinbami80@yahoo.com>
2023-03-22 09:48:00 -0700

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a string to the
+ * standard output followed by a new line
+ * Author - Akinbami Toafeek Olamide
+ * Date - 22nd of March, 2023.
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/EEADME.md
@@ -0,0 +1,1 @@
+Deep dive into Functions and Nested loops
