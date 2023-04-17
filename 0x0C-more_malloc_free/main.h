106803fa14d60886d589c11cc40f6ad7c3301cfa
Akinbami Taofeek Olamide <tfkakinbami80@yahoo.com>
2023-04-16 19:41:45 -0700

    a function that allocates memory using malloc.

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,7 @@ void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
+#include <stdio.h>
+#include <stdlib.h>
 
 #endif
