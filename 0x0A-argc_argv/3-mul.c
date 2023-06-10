10067c79c7cc3a30adcb8b33bfe9ccdeade6dad7
Akinbami Taofeek <tfkakinbami80@yahoo.com>
2023-06-09 18:49:21 -0700

    Modification on betty style

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,23 +6,23 @@
  * main - Starting point of our program.
  * @argc: Argument count
  * @argv: Argument Vector.
- * Return: Multiplacation of the command line 
+ * Return: Multiplacation of the command line
  */
 
 int main(int argc, char **argv)
 {
 	int num1, num2, result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	result = num1 * num2;
-	
 	printf("%d\n", result);
-	
-	return 0;
+
+	return (0);
 }
