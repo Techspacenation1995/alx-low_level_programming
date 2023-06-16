f0864eb99b018958845600d4c29b978b453d3e98
Akinbami Taofeek <tfkakinbami80@yahoo.com>
2023-06-15 19:09:55 -0700

    A dynamically allocated fucntion that concatenate a string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * str_concat - A dynamically allocated function that concatenate two strings
+ * @s1: String literals 1
+ * @s2: String literals 2
+ *
+ * Return: The concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *concat_String;
+
+	if ((s1 == NULL || s2 == NULL)) /* Condition when null is passed */
+	{
+		return ("");
+	}
+
+	/* Calculate the length of string 1 and string 2 */
+
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0')
+		len2++;
+
+	/* Allocate memory for the concatenated string */
+
+	concat_String = malloc((len1 + len2 + 1) * sizeof(char));
+
+	if (concat_String == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len1; i++) /* Copy s1 to concatenated string */
+		concat_String[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+		concat_String[i + j] = s2[j];
+
+	return (concat_String);
+}
+
