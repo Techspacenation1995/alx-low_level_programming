71920c86f125fed92c84f13c15a8d55ec612c542
Akinbami Taofeek <tfkakinbami80@yahoo.com>
2023-06-19 05:02:34 -0700

    a program that prints the name of the file it was compiled from, followed by a new line.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the name of the file to standard output
+ * Author - Akinbami Taofeek
+ * Return: The name of the file.
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
