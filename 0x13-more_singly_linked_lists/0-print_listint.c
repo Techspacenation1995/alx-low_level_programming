fb4260289a3d03c4fa265087b1b428195fcc9b04
Akinbami Taofeek <tfkakinbami80@yahoo.com>
2023-07-05 15:22:53 -0700

    Modification of code

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,9 +3,10 @@
 
 /**
  * print_listint - A function that prints the elment of a singly list.
- * @h - A pointer to a structure.
+ * @h: A pointer to a structure.
  *
- * Return the number of nodes in the string.
+ * Return: the number of nodes in the string.
+ * @return size_t
  */
 
 size_t print_listint(const listint_t *h)
@@ -15,8 +16,8 @@ size_t print_listint(const listint_t *h)
 	while (h != NULL)
 	{
 		count++;
-		printf("%d\n", h -> n);
-		h = h -> next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (count);
 }
