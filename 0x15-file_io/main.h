0ae81bc1f6aa693c228c5ceff39021abe311f15b
Akinbami Taofeek <tfkakinbami80@yahoo.com>
2023-07-11 05:53:22 -0700

    Updated main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stddef.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int _strlen(const char *s);
+int create_file(const char *filename, char *text_content);
 
 #endif
