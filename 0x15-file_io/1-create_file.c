5f81c39f0cc50eef91dca115dac2e570d464ee6d
Akinbami <tfkakinbami80@yahoo.com>
2023-08-07 01:52:36 -0700

    All task on File Handling in C

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file.
+ * @filename: pointer to the name of the file to create.
+ * @text_content: pointer to the null terminated string to write to file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+	int wwrite;
+
+	/* checks if filename is NULL */
+	if (filename == NULL)
+		return (-1);
+
+	/* if text_content is NULL create an empty file */
+	if (text_content == NULL)
+		text_content = "";
+
+	/* Open the file in write mode only, and truncate if exist */
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	/* If opening fails */
+	if (file_descriptor == -1)
+		return (-1);
+
+	/* write into file */
+	wwrite = write(file_descriptor, text_content, _strlen(text_content));
+
+	/* if write fails */
+	if (wwrite == -1)
+		return (-1);
+
+	/* close file */
+	close(file_descriptor);
+
+	return (1);
+}
