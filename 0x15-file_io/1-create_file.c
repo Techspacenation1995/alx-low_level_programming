a856ee93e68deb6110e0f9effbc0055fb0b9455f
Akinbami <tfkakinbami80@yahoo.com>
2023-08-07 04:50:37 -0700

    file updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -50,9 +50,9 @@ int create_file(const char *filename, char *text_content)
 
 int _strlen(const char *s)
 {
-        int len = 0;
+	int len = 0;
 
-        while (s[len] != '\0')
-                len++;
-        return (len);
+	while (s[len] != '\0')
+		len++;
+	return (len);
 }
