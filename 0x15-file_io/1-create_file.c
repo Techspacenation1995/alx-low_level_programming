709bd9157271a2d8ad74950324eaaff2b85ed8ec
Akinbami Taofeek <tfkakinbami80@yahoo.com>
2024-02-17 00:25:59 +0100

    a function that create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,58 +1,38 @@
 #include "main.h"
 
 /**
- * create_file - a function that creates a file.
- * @filename: pointer to the name of the file to create.
- * @text_content: pointer to the null terminated string to write to file.
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 if it success. -1 if it fails.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	int wwrite;
+	int fd;
+	int nletters;
+	int rwr;
 
-	/* checks if filename is NULL */
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	/* if text_content is NULL create an empty file */
-	if (text_content == NULL)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
 		text_content = "";
 
-	/* Open the file in write mode only, and truncate if exist */
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-	/* If opening fails */
-	if (file_descriptor == -1)
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
 
-	/* write into file */
-	wwrite = write(file_descriptor, text_content, _strlen(text_content));
-
-	/* if write fails */
-	if (wwrite == -1)
-		return (-1);
-
-	/* close file */
-	close(file_descriptor);
+	close(fd);
 
 	return (1);
-}
-
-/**
- * _strlen - Function that returns the length of string.
- * @s: pointer to the null-terminated byte string to be examined.
- *
- * Return: The length of the null-terminated byte string s.
- */
-
-int _strlen(const char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-		len++;
-	return (len);
 }
